e5d70ddc0f4c89f0ebe18d5e9205dcd008bf6ae4
Adam Menges <adam@adammenges.com>
2020-05-20 14:46:01 -0700

    starter project

---
+++ Lobe_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lobe_iOS
+//
+//  Created by Adam Menges on 5/20/20.
+//  Copyright © 2020 Adam Menges. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
