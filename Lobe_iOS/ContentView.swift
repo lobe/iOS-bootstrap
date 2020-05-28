9bcd6d003bceb7e53badf6b10eb65bb196c6ee7f
gaoyuezhou <zhougy_99@berkeley.edu>
2020-05-28 09:55:31 -0700

    buttons

--- Lobe_iOS/ContentView.swift
+++ Lobe_iOS/ContentView.swift
@@ -10,61 +10,152 @@ import SwiftUI
 import AVKit
 import Vision
 
+var useCam: Bool = true
 struct ContentView: View {
     var controller: MyViewController = MyViewController()
+    @State var showImagePicker: Bool = false
+    @State private var image: UIImage?
     var body: some View {
-        VStack {
-            MyRepresentable(controller: controller)
-            UpdateTextViewExternal(viewModel: controller)
+        GeometryReader { geometry in
+        
+            VStack {
+                if (self.image != nil) {
+                    Image(uiImage: self.image!)
+                                       .resizable()
+                        .aspectRatio(self.image!.size, contentMode: .fit)
+                } else {
+                    MyRepresentable(controller: self.controller)
+                }
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            .background(Color.black)
+            .edgesIgnoringSafeArea(.top)
+            
+            VStack  {
+                Spacer()
+                HStack {
+                        UpdateTextViewExternal(viewModel: self.controller)
+                        VStack{
+                            Button(action: {
+                                if self.image != nil {
+                                    self.controller.changeStatus(useCam: true, img: self.image!)
+                                    self.image = nil
+                                }
+                                self.controller.flipCamera()
+                            }) {
+                                Text("Switch Camera")
+                               .font(.system(size: 20))
+                           }
+                            Spacer()
+                           Button(action: {
+                               self.showImagePicker = true
+                           }) {
+                               Text("Photo Library")
+                               .font(.system(size: 20))
+                           }
+                        }
+                    }
+                .padding()
+                .frame(width: geometry.size.width,
+                      height: geometry.size.height/10, alignment: .bottomTrailing)
+//                    .background(Color.purple)
+                .sheet(isPresented: self.$showImagePicker) {
+                    ImagePicker(image: self.$image, isShown: self.$showImagePicker, hehe: self.controller, sourceType: .photoLibrary)}
+            }
         }
-        .edgesIgnoringSafeArea(.top)
     }
 }
 
 struct UpdateTextViewExternal: View {
     @ObservedObject var viewModel: MyViewController
+    @State var showImagePicker: Bool = false
+    @State private var image: UIImage?
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                VStack(alignment: .leading) {
-                    Spacer()
+                Spacer()
+                
+                HStack {
                     Text(self.viewModel.classificationLabel ?? "default")
-                        .foregroundColor(.black)
+                        .foregroundColor(.white)
                         .font(.system(size: 40))
                         .multilineTextAlignment(.leading)
-                }
+                        .frame(width: geometry.size.width/3,
+                            height: geometry.size.width/9, alignment: .bottomLeading)
             }
-            .padding()
             .frame(width: geometry.size.width,
-                   height: nil, alignment: .leading)
+                   height: nil, alignment: .bottomLeading)
         }
+            
     }
+}
 }
 
 class MyViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, ObservableObject {
 
     @Published var classificationLabel: String?
-    //    var myLabel: UILabel!
+    var backCam: AVCaptureDevice?
+    var frontCam: AVCaptureDevice?
+    var captureDevice: AVCaptureDevice?
+    var captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var useCam: Bool = true
+    var img: UIImage?
+    
+    func flipCamera() {
+       captureSession.stopRunning()
+       previewLayer?.removeFromSuperlayer()
+       if captureDevice == backCam{
+           captureDevice = frontCam}
+       else {
+           captureDevice = backCam}
+        captureSession = AVCaptureSession()
+        guard let input = try? AVCaptureDeviceInput(device: self.captureDevice!) else {return}
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        setPreviewLayer()
+        setOutput()
+       }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let captureSession = AVCaptureSession()
-        
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        backCam = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices.first!
+        frontCam = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .front).devices.first!
+        captureDevice = backCam
+        guard let input = try? AVCaptureDeviceInput(device: self.captureDevice!) else {return}
         captureSession.addInput(input)
         captureSession.startRunning()
-        
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
-        
+        setPreviewLayer()
+        setOutput()
+    }
+    
+    func setPreviewLayer() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer!.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        view.layer.addSublayer(previewLayer!)
+        previewLayer!.frame = view.frame
+    }
+    
+    func setOutput() {
         let dataOutput = AVCaptureVideoDataOutput()
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(dataOutput)
-        
+    }
+    
+    func getDevice(position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        let cam = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: position).devices
+        return cam.first
+    }
+    
+    func changeStatus(useCam: Bool, img: UIImage){
+        if useCam {
+            self.useCam = true
+            self.img = nil
+        } else {
+            self.useCam = false
+            self.img = img
+        }
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
@@ -74,7 +165,11 @@ class MyViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
             self.processClassifications(for: finishReq, error: err)
         }
         
-        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        if useCam {
+            try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        } else {
+            try? VNImageRequestHandler(ciImage: CIImage(cgImage: (self.img?.cgImage!)!)).perform([request])
+        }
         
     }
     
---
+++ Lobe_iOS/ImagePicker.swift
@@ -0,0 +1,68 @@
+//
+//  ImagePicker.swift
+//  Lobe_iOS
+//
+//  Created by Kathy Zhou on 5/27/20.
+//  Copyright © 2020 Adam Menges. All rights reserved.
+//
+
+
+import Foundation
+import SwiftUI
+
+class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @Binding var image: UIImage?
+    @Binding var isShown: Bool
+    var hehe: MyViewController
+    
+    init(image: Binding<UIImage?>, isShown: Binding<Bool>, hehe: MyViewController) {
+        _image = image
+        _isShown = isShown
+        self.hehe = hehe
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            image = uiImage
+            isShown = false
+            hehe.changeStatus(useCam: false, img: uiImage)
+        }
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        isShown = false
+    }
+    
+}
+
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    typealias UIViewControllerType = UIImagePickerController
+    typealias Coordinator = ImagePickerCoordinator
+    
+    @Binding var image: UIImage?
+    @Binding var isShown: Bool
+    var hehe: MyViewController
+    var sourceType: UIImagePickerController.SourceType = .camera
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+    }
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        return ImagePickerCoordinator(image: $image, isShown: $isShown, hehe: hehe)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+        
+    }
+    
+}
