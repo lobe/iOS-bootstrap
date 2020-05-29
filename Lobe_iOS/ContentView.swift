fddf97938f35e34a50ab1acdd796a0026bda40ab
gaoyuezhou <44328051+gaoyuezhou@users.noreply.github.com>
2020-05-29 12:50:28 -0700

    Kathy (#4)
    * buttons, green bar, layout redesign; TODO:screenshot

--- Lobe_iOS/ContentView.swift
+++ Lobe_iOS/ContentView.swift
@@ -10,74 +10,209 @@ import SwiftUI
 import AVKit
 import Vision
 
+var useCam: Bool = true
+
 struct ContentView: View {
     var controller: MyViewController = MyViewController()
+    @State var showImagePicker: Bool = false
+    @State private var image: UIImage?
+    
     var body: some View {
-        VStack {
-            MyRepresentable(controller: controller)
-            UpdateTextViewExternal(viewModel: controller)
+        GeometryReader { geometry in
+        
+            VStack {
+                if (self.image != nil) {
+                    Image(uiImage: self.image!)
+                                       .resizable()
+                        .aspectRatio(self.image!.size, contentMode: .fit)
+                } else {
+                    MyRepresentable(controller: self.controller)
+                }
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            .background(Color.black)
+            .edgesIgnoringSafeArea(.top)
+            
+            VStack  {
+                Spacer()
+                UpdateTextViewExternal(viewModel: self.controller)
+                HStack {
+                            Button(action: {
+                                self.showImagePicker = true
+                            }) {
+                                Text("Photo Lib")
+                                 .frame(width: geometry.size.width/16, height: geometry.size.width/16)
+                                    .multilineTextAlignment(.center)
+                            }
+                            .buttonStyle(RoundStyle())
+
+                    
+                            Button(action: {
+                                self.screenShotMethod()
+                            }) {
+                                Text("Screenshot")
+                                    .multilineTextAlignment(.center)
+                                .frame(width: geometry.size.width/9, height: geometry.size.width/9)
+                            }
+                            .buttonStyle(RoundStyle())
+                    
+                            Button(action: {
+                                if self.image != nil {
+                                    self.controller.changeStatus(useCam: true, img: self.image!)
+                                    self.image = nil
+                                }
+                                self.controller.flipCamera()
+                            }) {
+                                Text("Switch Cam")
+                                    .multilineTextAlignment(.center)
+                                    .frame(width: geometry.size.width/16, height: geometry.size.width/16)
+                           }
+                            .buttonStyle(RoundStyle())
+
+                    }
+                .padding()
+                .frame(width: geometry.size.width,
+                      height: nil, alignment: .bottom)
+                .sheet(isPresented: self.$showImagePicker) {
+                    ImagePicker(image: self.$image, isShown: self.$showImagePicker, controller: self.controller, sourceType: .photoLibrary)}
+            }
         }
-        .edgesIgnoringSafeArea(.top)
     }
+    
+
+    struct RoundStyle: ButtonStyle {
+        func makeBody(configuration: Self.Configuration) -> some View {
+            configuration.label
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.gray)
+            .mask(Circle())
+            .overlay(
+                Circle().stroke(Color("lightGray"), lineWidth: 6))
+            .shadow(radius: 10)
+        }
+    }
+    func screenShotMethod() {
+        let layer = UIApplication.shared.keyWindow!.layer
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+
+        layer.render(in: UIGraphicsGetCurrentContext()!)
+        let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        UIImageWriteToSavedPhotosAlbum(screenshot!, nil, nil, nil)
+    }
+    
 }
 
 struct UpdateTextViewExternal: View {
     @ObservedObject var viewModel: MyViewController
+    @State var showImagePicker: Bool = false
+    @State private var image: UIImage?
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                VStack(alignment: .leading) {
-                    Spacer()
-                    Text(self.viewModel.classificationLabel ?? "default")
-                        .foregroundColor(.black)
-                        .font(.system(size: 40))
-                        .multilineTextAlignment(.leading)
-                }
+                Spacer()
+                HStack {
+                    ZStack (alignment: .leading) {
+                        
+                        Rectangle().frame(width: min(CGFloat(self.viewModel.confidence ?? 0)*geometry.size.width, geometry.size.width), height: geometry.size.height/9)
+                            .foregroundColor(Color("deeperGreen"))
+                        .animation(.linear)
+                        
+                        Text(self.viewModel.classificationLabel ?? "default")
+                                               .foregroundColor(.white)
+                                               .font(.system(size: 40))
+                                               .frame(width: geometry.size.width, height: geometry.size.height/9, alignment: .center)
+                                           .background(Color("myGreen"))
+    
+                    }
+                   
             }
-            .padding()
             .frame(width: geometry.size.width,
-                   height: nil, alignment: .leading)
+                   height: geometry.size.height/7, alignment: .center)
         }
     }
+}
 }
 
 class MyViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, ObservableObject {
 
     @Published var classificationLabel: String?
-    //    var myLabel: UILabel!
+    var backCam: AVCaptureDevice?
+    var frontCam: AVCaptureDevice?
+    var captureDevice: AVCaptureDevice?
+    var captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var useCam: Bool = true
+    var img: UIImage?
+    var confidence: Float?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let captureSession = AVCaptureSession()
-        
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+    func flipCamera() {
+       captureSession.stopRunning()
+       previewLayer?.removeFromSuperlayer()
+       if captureDevice == backCam{
+           captureDevice = frontCam}
+       else {
+           captureDevice = backCam}
+        captureSession = AVCaptureSession()
+        guard let input = try? AVCaptureDeviceInput(device: self.captureDevice!) else {return}
         captureSession.addInput(input)
         captureSession.startRunning()
-        
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
-        
+        setPreviewLayer()
+        setOutput()
+       }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        backCam = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices.first!
+        frontCam = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .front).devices.first!
+        captureDevice = backCam
+        guard let input = try? AVCaptureDeviceInput(device: self.captureDevice!) else {return}
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        setPreviewLayer()
+        setOutput()
+    }
+    func setPreviewLayer() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer!.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        view.layer.addSublayer(previewLayer!)
+        previewLayer!.frame = view.frame
+    }
+    func setOutput() {
         let dataOutput = AVCaptureVideoDataOutput()
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(dataOutput)
-        
     }
-    
+    func getDevice(position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        let cam = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: position).devices
+        return cam.first
+    }
+    func changeStatus(useCam: Bool, img: UIImage){
+        if useCam {
+            self.useCam = true
+            self.img = nil
+        } else {
+            self.useCam = false
+            self.img = img
+        }
+    }
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
         guard let model = try? VNCoreMLModel(for: LobeModel().model) else { return }
         let request = VNCoreMLRequest(model: model) { (finishReq, err) in
             self.processClassifications(for: finishReq, error: err)
         }
-        
-        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
-        
+
+        if useCam {
+            try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        } else {
+            try? VNImageRequestHandler(ciImage: CIImage(cgImage: (self.img?.cgImage!)!)).perform([request])
+        }
+
     }
-    
     func processClassifications(for request: VNRequest, error: Error?) {
         DispatchQueue.main.async {
             guard let results = request.results else {
@@ -92,17 +227,17 @@ class MyViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
                 // Display top classifications ranked by confidence in the UI.
                 let topClassifications = classifications.prefix(1)
                 self.classificationLabel = topClassifications[0].identifier
+                self.confidence = topClassifications[0].confidence
             }
         }
     }
 }
 
-struct MyRepresentable: UIViewControllerRepresentable {
+struct MyRepresentable: UIViewControllerRepresentable{
     @State var controller: MyViewController
     func makeUIViewController(context: Context) -> MyViewController {
         return self.controller
     }
-    
     func updateUIViewController(_ uiViewController: MyViewController, context: Context) {
         
     }
