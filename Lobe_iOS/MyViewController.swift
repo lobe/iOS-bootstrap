bbc0e77465ef3e3ceb0782951383d961a6e985e4
Elliot Boschwitz <elbosc@microsoft.com>
2020-11-14 18:53:43 -1000

    Added support for changing model (#2)
    * gets project data on  localhost
    
    * project reorganization
    
    * updated project settings
    
    * Loading document picker
    
    * DocumentPicker integration for selecting model
    
    * Removed project list view
    
    * Removed unused changeProject handler
    
    * Removed DataProvider
    
    * Support for model name change
    
    * fixed preview
    
    * Minor styling changes to project button
    
    * Removed folders
    
    * Code cleanup

--- Lobe_iOS/MyViewController.swift
+++ Lobe_iOS/MyViewController.swift
@@ -256,9 +256,3 @@ extension UIImage {
         return self
     }
 }
-
-struct MyViewController_Previews: PreviewProvider {
-    static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
-    }
-}
