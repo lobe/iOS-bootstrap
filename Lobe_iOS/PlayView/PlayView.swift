8738b4eeda9092b0c7d7fd144e73f03bd78c32f1
Elliot Boschwitz <elbosc@microsoft.com>
2020-11-25 22:19:04 -1000

    File reorganization

--- Lobe_iOS/PlayView/PlayView.swift
+++ Lobe_iOS/PlayView/PlayView.swift
@@ -141,7 +141,7 @@ struct ContentView: View {
                 .offset(x: 0, y: self.showImagePicker ? 0: UIApplication.shared.keyWindow?.frame.height ?? 0)
         }.statusBar(hidden: true)
         .navigationBarBackButtonHidden(true)
-        .navigationBarItems(trailing: openScreenButton)
+        .navigationBarItems(leading: openScreenButton)
     }
 }
 
