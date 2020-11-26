73610f253958ad2c6eca78ff3b23c26d125a32ed
Elliot Boschwitz <elbosc@microsoft.com>
2020-11-25 22:01:49 -1000

    Set tint color to green

--- Lobe_iOS/SceneDelegate.swift
+++ Lobe_iOS/SceneDelegate.swift
@@ -27,6 +27,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: contentView)
             self.window = window
+            self.window?.tintColor = UIColor(rgb: 0x00DDAD)
             window.makeKeyAndVisible()
         }
     }
